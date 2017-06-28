cb143ea2b2b39125ba791b0c61aee68c6aaf9147
Yang.Bo <Yang.Bo@YANGBO-PC>
2017-06-28 10:47:48 +0800

    add code

---
+++ Signalr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Signalr.Startup))]
+namespace Signalr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
